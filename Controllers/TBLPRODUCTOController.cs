dfb794f763d9fc013bb29beefa0c010fcb6c318f
AnaMaria1799 <anamaria17123@gmail.com>
2025-05-20 19:12:59 -0500

    commit

--- Controllers/TBLPRODUCTOController.cs
+++ Controllers/TBLPRODUCTOController.cs
@@ -28,5 +28,64 @@ namespace PRACTICA_AEAE_3.Controllers
                 return View();
             }
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Nuevo(TBLPRODUCTO model)
+        {
+            if (ModelState.IsValid)
+            {
+                using (BDFACTURACION db = new BDFACTURACION())
+                {
+                    db.TBLPRODUCTO.Add(model);
+                    db.SaveChanges();
+                    TempData["Mensaje"] = "Producto creado.";
+                }
+                return RedirectToAction("Index");
+            }
+            ViewBag.IdCategoria = new SelectList(new BDFACTURACION().TBLCATEGORIA, "IdCategoria", "strNombre");
+            return View(model);
+        }
+
+        public ActionResult Editar(int id)
+        {
+            using (BDFACTURACION db = new BDFACTURACION())
+            {
+                ViewBag.IdCategoria = new SelectList(db.TBLCATEGORIA, "IdCategoria", "strNombre");
+                return View(db.TBLPRODUCTO.Find(id));
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Editar(TBLPRODUCTO model)
+        {
+            if (ModelState.IsValid)
+            {
+                using (BDFACTURACION db = new BDFACTURACION())
+                {
+                    db.Entry(model).State = EntityState.Modified;
+                    db.SaveChanges();
+                    TempData["Mensaje"] = "Producto actualizado.";
+                }
+                return RedirectToAction("Index");
+            }
+            ViewBag.IdCategoria = new SelectList(new BDFACTURACION().TBLCATEGORIA, "IdCategoria", "strNombre");
+            return View(model);
+        }
+
+        [HttpGet]
+        public ActionResult Borrar(int id)
+        {
+            using (BDFACTURACION db = new BDFACTURACION())
+            {
+                var producto = db.TBLPRODUCTO.Find(id);
+                db.TBLPRODUCTO.Remove(producto);
+                db.SaveChanges();
+                TempData["Mensaje"] = "Producto eliminado.";
+            }
+            return RedirectToAction("Index");
+        }
     }
+
 }
