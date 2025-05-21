456a9b818a0062ce24670b83a0d2ffbb0eaa32cd
AnaMaria1799 <anamaria17123@gmail.com>
2025-05-20 19:09:44 -0500

    commit

---
+++ Controllers/TBLPRODUCTOController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PRACTICA_AEAE_3.Models;
+using System.Data.Entity;
+
+namespace PRACTICA_AEAE_3.Controllers
+{
+    public class TBLPRODUCTOController : Controller
+    {
+        // GET: TBLPRODUCTO
+        public ActionResult Index()
+        {
+            using (BDFACTURACION db = new BDFACTURACION())
+            {
+                ViewBag.IdCategoria = new SelectList(db.TBLCATEGORIA, "IdCategoria", "strNombre");
+                return View(db.TBLPRODUCTO.Include(p => p.TBLCATEGORIA).ToList());
+            }
+        }
+
+        public ActionResult Nuevo()
+        {
+            using (BDFACTURACION db = new BDFACTURACION())
+            {
+                ViewBag.IdCategoria = new SelectList(db.TBLCATEGORIA, "IdCategoria", "strNombre");
+                return View();
+            }
+        }
+    }
+}
